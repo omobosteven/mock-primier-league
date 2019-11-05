4bd291b9e727861c07882a4795b5e92125fc6268
omobosteven <omobosteven@gmail.com>
2019-11-05 15:54:23 +0100

    chore: setup express project - setup express server - setup eslint - setup travis - setup coveralls

---
+++ src/tests/root.test.js
@@ -0,0 +1,20 @@
+import request from 'supertest';
+import app from '../app';
+
+describe('Test root Endpoint', () => {
+    it('should return message on root endpoint', async (done) => {
+        const res = await request(app)
+                            .get('/')
+        expect(res.status).toEqual(200)
+        expect(res.body.message).toBe('mock primier league!!!')
+        done();
+    })
+
+    it('should return error if route does not exist', async (done) => {
+        const res = await request(app)
+                            .get('/ksfjd')
+        expect(res.status).toEqual(404)
+        expect(res.body.message).toBe('Route GET /ksfjd not found.')
+        done();
+    })
+  });
