193f3b43a8e18812ddd30d238a8091d9dedf156a
omobosteven <omobosteven@gmail.com>
2019-11-14 21:03:30 +0100

    implement API rate limit for user account

---
+++ src/middlewares/rateLimit.js
@@ -0,0 +1,38 @@
+import rateLimit from 'express-rate-limit';
+import RedisStore from 'rate-limit-redis';
+import Redis from 'ioredis';
+
+const client = new Redis(process.env.REDIS_URL);
+const max = parseInt(process.env.RATE_LIMIT_MAX, 10) || 20;
+const minutesBeforeLimit = parseInt(process.env.RATE_LIMIT_MINUTES, 10) || 1;
+
+
+const limiter = rateLimit({
+    store: new RedisStore({
+        client,
+        expiry: minutesBeforeLimit * 60
+    }),
+    max,
+    skip: (req) => {
+        const {
+            admin
+        } = req.user;
+
+        return admin;
+    },
+    keyGenerator: (req) => {
+        const {
+            user
+        } = req.user;
+
+        return user;
+    },
+    handler: (req, res) => {
+        return res.status(429).send({
+            status: 429,
+            message: 'Too many request, please try again later.'
+        });
+    }
+});
+
+export default limiter;
---
+++ src/tests/setUpTest.js
@@ -0,0 +1,2 @@
+process.env.RATE_LIMIT_MAX = 100;
+process.env.RATE_LIMIT_MINUTES = 60;
